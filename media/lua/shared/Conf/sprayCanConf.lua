597c543ac0ae71d9561b79b4638dcdbdf7375170
Thuztor <thuztor@gmx.de>
2015-09-04 14:04:47 +0200

    new colour config

--- media/lua/shared/Conf/sprayCanConf.lua
+++ media/lua/shared/Conf/sprayCanConf.lua
@@ -7,24 +7,24 @@ sprayCanConf={};
 
 sprayCanConf.list={
 	{ name = 'SpraycanWhite',  red = 0.914, green = 0.914, blue = 0.914, text = getText('UI_InWhite')  },
-	{ name = 'SpraycanRed',    red = 0.702, green = 0.294, blue = 0.282, text = getText('UI_InRed')    },
-	{ name = 'SpraycanBlue',   red = 0.353, green = 0.416, blue = 0.714, text = getText('UI_InBlue')   },
-	{ name = 'SpraycanGreen',  red = 0.420, green = 0.714, blue = 0.345, text = getText('UI_InGreen')  },
-	{ name = 'SpraycanOrange', red = 0.886, green = 0.655, blue = 0.220, text = getText('UI_InOrange') },
-	{ name = 'SpraycanViolet', red = 0.659, green = 0.110, blue = 0.843, text = getText('UI_InViolet') },
-	{ name = 'SpraycanYellow', red = 0.804, green = 0.843, blue = 0.102, text = getText('UI_InYellow') },
+	{ name = 'SpraycanRed',    red = 0.901, green = 0.157, blue = 0.118, text = getText('UI_InRed')    },
+	{ name = 'SpraycanBlue',   red = 0.176, green = 0.294, blue = 0.804, text = getText('UI_InBlue')   },
+	{ name = 'SpraycanGreen',  red = 0.235, green = 0.667, blue = 0.118, text = getText('UI_InGreen')  },
+	{ name = 'SpraycanOrange', red = 0.941, green = 0.667, blue = 0.118, text = getText('UI_InOrange') },
+	{ name = 'SpraycanViolet', red = 0.549, green = 0.078, blue = 0.863, text = getText('UI_InViolet') },
+	{ name = 'SpraycanYellow', red = 0.902, green = 0.902, blue = 0.137, text = getText('UI_InYellow') },
 	{ name = 'SpraycanCyan',   red = 0.125, green = 0.8,   blue = 0.784, text = getText('UI_InCyan')   },
 	{ name = 'SpraycanBlack',  red = 0.224, green = 0.224, blue = 0.224, text = getText('UI_InBlack')  },
 };
 
 sprayCanConf.listChalk={
 	{ name = 'ChalkWhite',  red = 0.914, green = 0.914, blue = 0.914, text = getText('UI_InWhite')  },
-	{ name = 'ChalkRed',    red = 0.702, green = 0.294, blue = 0.282, text = getText('UI_InRed')    },
-	{ name = 'ChalkBlue',   red = 0.353, green = 0.416, blue = 0.714, text = getText('UI_InBlue')   },
-	{ name = 'ChalkGreen',  red = 0.420, green = 0.714, blue = 0.345, text = getText('UI_InGreen')  },
-	{ name = 'ChalkOrange', red = 0.886, green = 0.655, blue = 0.220, text = getText('UI_InOrange') },
-	{ name = 'ChalkViolet', red = 0.659, green = 0.110, blue = 0.843, text = getText('UI_InViolet') },
-	{ name = 'ChalkYellow', red = 0.804, green = 0.843, blue = 0.102, text = getText('UI_InYellow') },
+	{ name = 'ChalkRed',    red = 0.901, green = 0.157, blue = 0.118, text = getText('UI_InRed')    },
+	{ name = 'ChalkBlue',   red = 0.176, green = 0.294, blue = 0.804, text = getText('UI_InBlue')   },
+	{ name = 'ChalkGreen',  red = 0.235, green = 0.667, blue = 0.118, text = getText('UI_InGreen')  },
+	{ name = 'ChalkOrange', red = 0.941, green = 0.667, blue = 0.118, text = getText('UI_InOrange') },
+	{ name = 'ChalkViolet', red = 0.549, green = 0.078, blue = 0.863, text = getText('UI_InViolet') },
+	{ name = 'ChalkYellow', red = 0.902, green = 0.902, blue = 0.137, text = getText('UI_InYellow') },
 	{ name = 'ChalkCyan',   red = 0.125, green = 0.8,   blue = 0.784, text = getText('UI_InCyan')   },
 	{ name = 'ChalkBlack',  red = 0.224, green = 0.224, blue = 0.224, text = getText('UI_InBlack')  },
 };
